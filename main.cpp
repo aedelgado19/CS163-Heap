5689ecb7e3e774f4a7933eefd072af224ad7af67
aedelgado19 <aedelgado19@gmail.com>
2020-11-27 23:20:53 -0800

    initial commit

---
+++ main.cpp
@@ -0,0 +1,11 @@
+/* Author: Allison Delgado
+ * Last Updated: November 28, 2020
+ * This program creates a heap to store up to 100 numbers
+ * and displays them visually
+ */
+
+int main(){
+
+
+  return 0;
+}
