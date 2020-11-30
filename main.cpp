3084b1b16d98aae8f98ae6f7a38be992ae579c64
aedelgado19 <aedelgado19@gmail.com>
2020-11-29 18:09:43 -0800

    wOt

--- main.cpp
+++ main.cpp
@@ -13,35 +13,36 @@ using namespace std;
 //function prototypes
 int openFile(int inputArray[SIZE]);
 int getManualNums(int inputArray[SIZE]);
-void insert(int inputArray[SIZE], int arraysize, int outputArray[arraysize]);
+void insert(int inputArray[SIZE], int arraysize, int &outputArray[]);
 void printTree(); //prints visual tree
 void printOutput(); //prints greatest to smallest
-void heapify(int arraysize, int outputArray[arraysize], int searchnum, int childIndex); 
-int findParent(int arraysize, int outputArray[arraysize], int childIndex);
+void heapify(int arraysize, int &outputArray[], int searchnum, int childIndex); 
+int findParent(int arraysize, int &outputArray[], int childIndex);
 void removeNode(); //removes from tree, adds to output
 
 //creates nodes for each slot of input array
-void insert(int inputArray[SIZE], int arraysize, int outputArray[arraysize]){
+void insert(int inputArray[SIZE], int arraysize, int outputArray[]){
   int j = 0; //keeps track of input array
   for(int i = 1; i < arraysize+1; i++){ //start at i = 1
     outputArray[i] = inputArray[j];
     j++;
     heapify(arraysize, outputArray, outputArray[i], i);
   }
+  
+  cout << "out of for loop: " << endl;
+  for(int j = 1; j < arraysize+1; j++){
+    cout << outputArray[j] << " ";
+  }
+
 }
 
-void heapify(int arraysize, int outputArray[arraysize], int searchnum, int childIndex){
+void heapify(int arraysize, int &outputArray[], int searchnum, int childIndex){
   int parentIndex = 0;
   int parentNum = 0;
   int temp = 0;
   parentIndex = findParent(arraysize, outputArray, childIndex);
-  cout << "a- parent index is: " << parentIndex << endl;
   parentNum = outputArray[parentIndex];
-  cout << "b- parent num is: " << parentNum << endl;
   while(searchnum > parentNum){
-    cout << "swapping numbers because " << searchnum << " > " << parentNum << endl;
-    cout << "before swap: parent num = " << parentNum << " at slot " << parentIndex << endl;
-    cout << "and child num = " << searchnum << " at slot " << childIndex << endl;
     //swap numbers
     temp = parentNum;
     parentNum = searchnum;
@@ -52,25 +53,17 @@ void heapify(int arraysize, int outputArray[arraysize], int searchnum, int child
     childIndex = parentIndex;
     parentIndex = temp;
 
-    cout << "after swap new 'parent' num: " << parentNum << " at slot: " << parentIndex << endl;
-    cout << "after swap new 'child' num: " << searchnum << " at slot: " << childIndex << endl;
-
     //recall functions
     parentIndex = findParent(arraysize, outputArray, parentIndex);
-    searchnum = outputArray[parentIndex];
-    cout << "recalled functions. new parent index is " << parentIndex << endl;
-    cout << "new searchnum (child) is " << searchnum << endl; 
+    searchnum = outputArray[parentIndex];  
   }
-  cout << "out of while loop" << endl;
 }
 
-int findParent(int arraysize, int outputArray[arraysize], int childIndex){
+int findParent(int arraysize, int &outputArray[], int childIndex){
   int parentIndex = 0;
-  cout << "inside findParent(). child index is " << childIndex << endl;
   parentIndex = childIndex/2;
   if(parentIndex == 0){
     parentIndex = 1; //special case
-    cout << "parent index was 0, has been changed to 1" << endl;
   }
   return parentIndex;
 }
