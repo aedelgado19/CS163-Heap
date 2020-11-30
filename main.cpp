86d1a84774b342b33a71cdaffb64eebd348c06aa
aedelgado19 <aedelgado19@gmail.com>
2020-11-29 17:25:23 -0800

    debugging heapify

--- main.cpp
+++ main.cpp
@@ -10,29 +10,57 @@
 using namespace std;
 #define SIZE 100
 
-struct Node {
-  int value; //num
-  Node *left; //left child
-  Node *right; //right child
-};
-
 //function prototypes
-void openFile(int inputArray[SIZE]);
-void getManualNums(int inputArray[SIZE]);
-Node* insert(int inputArray[SIZE]);
+int openFile(int inputArray[SIZE]);
+int getManualNums(int inputArray[SIZE]);
+void insert(int inputArray[SIZE], int arraysize, int outputArray[arraysize]);
 void printTree(); //prints visual tree
 void printOutput(); //prints greatest to smallest
-void heapify(); //recursively swaps nodes to order in heap 
+void heapify(int arraysize, int outputArray[arraysize], int searchnum, int childIndex); 
+int findParent(int arraysize, int outputArray[arraysize], int childIndex);
 void removeNode(); //removes from tree, adds to output
 
 //creates nodes for each slot of input array
-Node* insert(int inputArray[SIZE]){
+void insert(int inputArray[SIZE], int arraysize, int outputArray[arraysize]){
+  int j = 0; //keeps track of input array
+  for(int i = 1; i < arraysize+1; i++){ //start at i = 1
+    outputArray[i] = inputArray[j];
+    j++;
+    heapify(arraysize, outputArray, outputArray[i], i);
+  }
+}
 
+void heapify(int arraysize, int outputArray[arraysize], int searchnum, int childIndex){
+  int parentIndex = 0;
+  int parentNum = 0;
+  int temp = 0;
+  
+  parentIndex = findParent(arraysize, outputArray, childIndex);
+  parentNum = outputArray[parentIndex];
+  while(searchnum > parentNum){
+    //swap numbers
+    temp = parentNum;
+    parentNum = searchnum;
+    searchnum = temp;
+    //swap indexes
+    temp = childIndex;
+    childIndex = parentIndex;
+    parentIndex = temp;
 
+    //recall functions
+    parentIndex = findParent(arraysize, outputArray, parentIndex);
+    searchnum = outputArray[parentIndex];
+  }
+}
+
+int findParent(int arraysize, int outputArray[arraysize], int childIndex){
+  int parentIndex = 0;
+  parentIndex = childIndex/2;
+  return parentIndex;
 }
 
 //gets numbers from a file
-void openFile(int inputArray[SIZE]){
+int openFile(int inputArray[SIZE]){
   ifstream inFile;
   int num = 0;
   int count = 0;
@@ -51,12 +79,12 @@ void openFile(int inputArray[SIZE]){
     inputArray[count] = num; //add into input array
     count++;
   }
+  return count;
 }
 
 //gets manually entered numbers from user
-void getManualNums(int inputArray[SIZE]){
+int getManualNums(int inputArray[SIZE]){
   char input[1000];
-  Node* numPtr = new Node;
   int num = 0;
   int amountOfNums = 0;
   int counter = 0;
@@ -70,25 +98,22 @@ void getManualNums(int inputArray[SIZE]){
     num = atoi(token);
     token = strtok(NULL, " ");    
     inputArray[counter] = num;
-    amountOfNums++; //used for debug print
+    amountOfNums++;
     counter++; //iterate in order to add into array
   }
-  /* debug:
-  cout << "print array: " << endl;
-  for(int j = 0; j < amountOfNums; j++){
-    cout << inputArray[j] << endl;
-  }
-  */
+  return amountOfNums;
 }
 
 int main(){
+  int arraysize = 0;
   int input = 0;
   int inputArray[SIZE];
-  Node* tree;
+  int outputArray[arraysize];
   bool validInput = false;
   cout << "*************************" << endl;
   cout << "Welcome to Heap!" << endl;
   while(validInput == false){
+    cout << "Please use the following numbers to indicate your response. " << endl;
     cout << "Would you like to input numbers through: " << endl;
     cout << "(1) a file" << endl;
     cout << "(2) the console" << endl;
@@ -97,13 +122,13 @@ int main(){
     cin.get();
     if(input == 1){ //numbers are in a file
       validInput = true;
-      openFile(inputArray);
-      tree = insert(inputArray);
+      arraysize = openFile(inputArray);
+      insert(inputArray, arraysize, outputArray);
     }
     else if(input == 2){ //manually input numbers
       validInput = true;
-      getManualNums(inputArray);
-      tree = insert(inputArray);
+      arraysize = getManualNums(inputArray);
+      insert(inputArray, arraysize, outputArray);
     }
     else { //neither 
       cout << "That was not a valid input." << endl;
